fbbb85115df7a24b95faa5cee575887cba764e5b
Truong quoc khanh <ktr>
2019-03-25 15:38:47 +0700

    Add distribute

--- app/src/main/java/com/example/truongquockhanh/myciproject/MainActivity.kt
+++ app/src/main/java/com/example/truongquockhanh/myciproject/MainActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCenter.start(application,BuildConfig.HOCKEY_APP_KEY, Analytics::class.java, Crashes::class.java)
+        AppCenter.start(
+            application, BuildConfig.HOCKEY_APP_KEY,
+            Analytics::class.java,
+            Crashes::class.java,
+            Distribute::class.java
+        )
     }
 }
